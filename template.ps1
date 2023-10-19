20ab32e5178e40240e3d92da085a55988797e103
JustATechie <louie@jhu.edu>
2023-10-19 16:54:22 -0400

    :construction: created template.ps1 for future scripts

---
+++ template.ps1
@@ -0,0 +1,45 @@
+<#
+    .SYNOPSIS
+    Script to <what will the script do>
+
+    .DESCRIPTION
+    This script will <Elaborate on what the script does>
+
+    .PARAMETER Param1
+    Specifies <What? Is the parameter required?>
+
+    .INPUTS
+    <Does the script accept an input>
+
+    .OUTPUTS
+    A log file in the temp directory of the user running the script
+
+    .NOTES
+    Version:        0.1
+    Author:         <Name>
+    Creation Date:  <Date>
+    Purpose/Change: Initial script development
+
+    .EXAMPLE
+    <Give multiple examples of the script if possible>
+#>
+
+#requires -version 3.0
+
+#-----------------------------------------------------------[Parameters]-----------------------------------------------------------
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+# Ensure best coding practices are followed
+Set-StrictMode -Version Latest
+
+# Set Error Action to Silently Continue
+$ErrorActionPreference = "Stop"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#-----------------------------------------------------------[Finish up]------------------------------------------------------------
