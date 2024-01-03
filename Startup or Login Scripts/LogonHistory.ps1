fcd719d2cc725462b60b0eff1a328e727a080125
JustATechie <louie@jhu.edu>
2024-01-03 10:25:40 -0500

    :memo: added a plethora of comments

--- Startup or Login Scripts/LogonHistory.ps1
+++ Startup or Login Scripts/LogonHistory.ps1
@@ -1,10 +1,13 @@
 <#
     .SYNOPSIS
-    This function has three ways of being used. It can be used to track logon history, logoff history, and can email logs to the specified addresses.
+    This function has three ways of being used. It can be used to track logon history, logoff history, and/or can email logs to the specified address.
 
     .DESCRIPTION
     Each time the function is run, it will perform it's setup, which involves setting up the log path, and replicating the script file locally
-    for the send email portion if used through a scheduled task.
+    for the send email portion if used through a scheduled task. Once complete, depending on the action parameter, will act accordingly.
+
+    .PARAMETER action
+    Required. Either "Logon", "Logoff", or "Email" to specific what the script should do.
 
     .NOTES
     Version:        1.0
@@ -15,6 +18,7 @@
 
 #-----------------------------------------------------------[Parameters]-----------------------------------------------------------
 param(
+    [Parameter(Mandatory=$true, Position=0)]
     [String]$action
 )
 #---------------------------------------------------------[Initialisations]--------------------------------------------------------
@@ -33,13 +37,17 @@ $folderPath="C:\Windows\Logs\USS-LogonHistory"
 $scriptPath=$folderPath + "\logonHistory.ps1"
 $todayLogPath=$folderPath + "\logonHistory - $(Get-Date -Date $today -UFormat "%m-%d-%Y") - $($env:computername).csv"
 $yesterdayLogPath=$folderPath + "\logonHistory - $(Get-Date -Date $yesterday -UFormat "%m-%d-%Y") - $($env:computername).csv"
-
-$SmtpServer='smtp.johnshopkins.edu'
-$From=[mailaddress]'ussitcloudapps@jhu.edu'
-$To=[mailaddress]'ldibern1@jh.edu'
+$smtpServer='smtp.johnshopkins.edu'
+$from=[mailaddress]'ussitcloudapps@jhu.edu'
+$to=[mailaddress]'ldibern1@jh.edu'
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
+<#
+    .DESCRIPTION
+    Checks to see if the log path exists and creates it if it doesn't. Also downloads a local copy of this script to be used by the task scheduler.
+    Additionally create today's logfile if it does not already exist.
+ #>
 function setupCheck {
     # If the log folder path does not exist, create it!
     If(-Not (Test-Path -Path $folderPath)) {
@@ -59,35 +67,56 @@ function setupCheck {
     }
 }
 
+<#
+    .DESCRIPTION
+    Creates today's logfile. Assumes that one does not already exist.
+ #>
 function createTodayLogFile {
     New-Item -Path $todayLogPath -ItemType "file"
     Set-Content -Path $todayLogPath -Value "date,time,type,jhed"
 }
 
+<#
+    .DESCRIPTION
+    Deletes yesterdays's logfile.
+ #>
 function deleteYesterdayLogFile {
     Remove-Item -Path $yesterdayLogPath
 }
 
-function handleAction{
+<#
+    .DESCRIPTION
+    Based on the action parameter, this function will either log the current user as logged in, log the current user and logged out, or email yesterday's logfile
+    and delete it once sent.
+ #>
+function handleAction {
     switch ($action)
     {
         "Logon" {
+            # Log the current user and logged on in today's logfile.
             Add-Content -Path $todayLogPath -Value "$(Get-Date -Date $today -UFormat "%m/%d/%Y"),$(Get-Date -UFormat "%R"),login,$Env:UserName"
         }
         "Logoff" {
+            # Log the current user and logged off in today's logfile.
             Add-Content -Path $todayLogPath -Value "$(Get-Date -Date $today -UFormat "%m/%d/%Y"),$(Get-Date -UFormat "%R"),logoff,$Env:UserName"
         }
         "Email" {
-            Send-MailMessage -From $From -To $To -Subject "$($env:computername): $(Get-Date -Date $yesterday -UFormat "%m/%d/%Y") Logs" -SmtpServer $SmtpServer -Attachments $yesterdayLogPath
+            # Email yesterday's logfile to the intended address.
+            Send-MailMessage -From $from -To $to -Subject "$($env:computername): $(Get-Date -Date $yesterday -UFormat "%m/%d/%Y") Logs" -SmtpServer $smtpServer -Attachments $yesterdayLogPath
 
-            #now that we've sent the log history, delete yesterday's logfile.
+            # Now that we've sent the log history, delete yesterday's logfile.
             deleteYesterdayLogFile
         }
         Default {
+            # do nothing.
         }
     }
 }
 
+<#
+    .DESCRIPTION
+    The main function calls all other functions to perform the actions required by this script.
+#>
 function main {
     setupCheck
     handleAction
