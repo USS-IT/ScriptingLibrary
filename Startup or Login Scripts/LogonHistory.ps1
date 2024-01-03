f725f9b9a433bcbc2e62b91fcd30c10e28c463a7
JustATechie <louie@jhu.edu>
2024-01-03 09:50:09 -0500

    :bug: fixed issue with main function not being called properly. Added some comments.

--- Startup or Login Scripts/LogonHistory.ps1
+++ Startup or Login Scripts/LogonHistory.ps1
@@ -1,4 +1,31 @@
-param([int]$action)
+<#
+    .SYNOPSIS
+    This function has three ways of being used. It can be used to track logon history, logoff history, and can email logs to the specified addresses.
+
+    .DESCRIPTION
+    Each time the function is run, it will perform it's setup, which involves setting up the log path, and replicating the script file locally
+    for the send email portion if used through a scheduled task.
+
+    .NOTES
+    Version:        1.0
+    Author:         LDG
+    Creation Date:  01/02/2024
+    Purpose/Change: Track logon/off histories on computers.
+#>
+
+#-----------------------------------------------------------[Parameters]-----------------------------------------------------------
+param(
+    [String]$action
+)
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+# Ensure best coding practices are followed
+Set-StrictMode -Version Latest
+
+# Set Error Action to Continue
+$ErrorActionPreference = "Continue"
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
 
 $today=$(Get-Date)
 $yesterday=$((Get-Date).AddDays(-1))
@@ -8,10 +35,12 @@ $todayLogPath=$folderPath + "\logonHistory - $(Get-Date -Date $today -UFormat "%
 $yesterdayLogPath=$folderPath + "\logonHistory - $(Get-Date -Date $yesterday -UFormat "%m-%d-%Y") - $($env:computername).csv"
 
 $SmtpServer='smtp.johnshopkins.edu'
-$From=[mailaddress]'ussitservices@jhu.edu'
+$From=[mailaddress]'ussitcloudapps@jhu.edu'
 $To=[mailaddress]'ldibern1@jh.edu'
 
-function setupCheck{
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+function setupCheck {
     # If the log folder path does not exist, create it!
     If(-Not (Test-Path -Path $folderPath)) {
         New-Item -Path "C:\Windows\Logs\" -Name "USS-LogonHistory" -ItemType "directory"
@@ -45,11 +74,11 @@ function handleAction{
         "Logon" {
             Add-Content -Path $todayLogPath -Value "$(Get-Date -Date $today -UFormat "%m/%d/%Y"),$(Get-Date -UFormat "%R"),login,$Env:UserName"
         }
-        "Log-off" {
+        "Logoff" {
             Add-Content -Path $todayLogPath -Value "$(Get-Date -Date $today -UFormat "%m/%d/%Y"),$(Get-Date -UFormat "%R"),logoff,$Env:UserName"
         }
-        "Email"{
-            Send-MailMessage -From $From -To $To -Subject "$($env:computername): $(Get-Date -Date $date -UFormat "%m/%d/%Y") Logs" -SmtpServer $SmtpServer -Attachments $yesterdayLogPath
+        "Email" {
+            Send-MailMessage -From $From -To $To -Subject "$($env:computername): $(Get-Date -Date $yesterday -UFormat "%m/%d/%Y") Logs" -SmtpServer $SmtpServer -Attachments $yesterdayLogPath
 
             #now that we've sent the log history, delete yesterday's logfile.
             deleteYesterdayLogFile
@@ -59,10 +88,14 @@ function handleAction{
     }
 }
 
-main {
+function main {
     setupCheck
     handleAction
 }
 
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+# Call the main function
 main
 
+#-----------------------------------------------------------[Finish up]------------------------------------------------------------
