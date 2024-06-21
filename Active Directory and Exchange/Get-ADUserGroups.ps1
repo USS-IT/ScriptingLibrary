89efd4430b93161af06bd1b0c4b0386c975391c0
mcarras8 <102685457+mcarras8@users.noreply.github.com>
2024-06-21 09:46:06 -0400

    Update Get-ADUserGroups.ps1
    - Add the ability to get all parent (recursive) group memberships, which is enabled by default.
    - Sort the results.

--- Active Directory and Exchange/Get-ADUserGroups.ps1
+++ Active Directory and Exchange/Get-ADUserGroups.ps1
@@ -3,21 +3,36 @@
 $username = Read-Host "Enter JHED"
 $outputFile = Read-Host "Output CSV file (leave blank to show in pop-up)"
 $ussOnly = Read-Host "USS OU Groups Only (Y/N, Default: Y)"
+$getRecursive = Read-Host "Get all parent groups (may take longer) (Y/N, Default: Y)"
 
-$results = Get-ADUser $username -Properties memberof | Select -ExpandProperty memberof
-if ($ussOnly -ne "N") {
-	$results = $results | where {$_ -like "*,OU=USS,*"}
-}
-$results = $results | foreach { if ($_ -match "CN=([^,]+),") { $matches[1] } }
-
-if (-Not [string]::IsNullOrEmpty($outputFile)) {
-	if($outputFile -notlike "*\*") {
-		$outputFile = "{0}\{1}" -f ${ENV:OneDrive}, $outputFile
+Write-Host "Fetching user..."
+$user = Get-ADUser $username -Properties distinguishedname,memberof
+if (-Not [string]::IsNullOrEmpty($user.distinguishedname)) {
+	Write-Host "Fetching groups, please wait..."
+	if ($getRecursive -ne "N") {
+		# Use tokenGroups to get all recursive groups
+		$results = ($user | Get-ADUser -Properties tokenGroups).tokenGroups | Get-ADGroup
+		if ($ussOnly -ne "N") {
+			$results = $results | where {$_.distinguishedname -like "*,OU=USS,*"}
+		}
+		$results = $results | Select Name,@{N="Parent Group"; Expression={ if($_ -in $user.memberof) { "" } else { "Y" } }} | Sort-Object -Property Name
+	} else {
+		$results = $user | Select -ExpandProperty memberof
+		if ($ussOnly -ne "N") {
+			$results = $results | where {$_ -like "*,OU=USS,*"}
+		}
+		$results = $results | foreach { if ($_ -match "CN=([^,]+),") { $matches[1] } } | Sort-Object
 	}
-	$results | Export-CSV -NoTypeInformation $outputFile
-	Write-Host "Exported to [$outputFile]"
-} else {
-	$results
-	$results | Out-GridView -Title "AD Groups for $username"
+	
+	if (-Not [string]::IsNullOrEmpty($outputFile)) {
+		if($outputFile -notlike "*\*") {
+			$outputFile = "{0}\{1}" -f ${ENV:OneDrive}, $outputFile
+		}
+		$results | Export-CSV -NoTypeInformation $outputFile
+		Write-Host "Exported to [$outputFile]"
+	} else {
+		$results
+		$results | Out-GridView -Title "AD Groups for $username"
+	}
 }
 Read-Host "Press enter to exit" | Out-Null
